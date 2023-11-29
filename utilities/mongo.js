215b966fca72e0ef12af8090bbbe2a9a41e3a731
Abhishek Salvi <123547817+abhishek-salvi-ds@users.noreply.github.com>
2023-11-29 15:28:49 +0530

    inital commit

--- utilities/mongo.js
+++ utilities/mongo.js
@@ -1,4 +1,6 @@
 module.exports = {
-  MongoURI: "mongodb://localhost:27017/file-management",
+  // MongoURI: "mongodb://localhost:27017/file-management",
+  MongoURI:
+    "mongodb+srv://abhishek:TeleSeason989@cluster0.tlalshw.mongodb.net/file-management",
   USERS_COLLECTION: "users",
 };
